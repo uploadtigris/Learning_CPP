99909a5f114aa29740a3c1a1faa01f8f88966c15
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-10-17 22:07:00 -0500

    Create Mendez_Ch7Lab.cpp

---
+++ Mendez_Ch7Lab.cpp
@@ -0,0 +1,128 @@
+//###########################################################################
+// *** Assignment is not Complete ***
+//
+// Lab 7.0, Version 1
+// Name (Developer): Tigris Mendez
+// Class: COSC~1337
+// Proffesor: Jorgenson
+// Date: 10/16/2021
+//
+// Purpose of Program:
+//      After retrieving the total price of a bill, this program will compute
+//      the necessary tip at a percentage also given by the user.
+//###########################################################################
+
+
+//some sudocode!!
+//
+// taxRate (private) set by a ONE parameter constructor
+            // when Tips object created, passes value to taxRate
+            // if no argument passed, the default is 0.085
+                    // use a default constructor for this value
+
+// Tips class
+    //has taxRate as a private member
+    //only One Public function, computeTip
+        //computeTip accepts TWO arguments.
+            //(total bill amount , and taxRate's stored value.)
+            // subtracts tax from meal cost with tax
+            // calculates gratuity from meal w/o tax included
+
+
+// main
+    // create an instance of Tips
+    // utilize the Tips object in a for loop
+        //at end of loop ask "another tip (y/n)?"
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+//making class variables public is not the best practice
+//An object is how you access stuff *inside the class
+
+class Tips
+{
+private:
+    float taxRate;
+    float amountTaxed;
+    float billWithOutTax;
+    float tip;
+
+public:
+
+    void computeTip(float totalBillAmount, float taxRate)
+    {
+        amountTaxed = totalBillAmount * (taxRate/100);
+        cout << amountTaxed << endl;
+        billWithOutTax = (totalBillAmount - amountTaxed);
+        cout << billWithOutTax << endl;
+        tip = billWithOutTax * (desiredTipRate / 100);
+        //cout << tip << endl;
+    }
+};
+
+float getTax() {
+    float tax;
+    cout << "Tax % for this location: ";
+    cin >> tax;
+
+    while (tax < 0) {
+        if (tax > 0) {
+            cin >> tax;
+        } else if (tax < 0) {
+            cout << "Tax % cannot be less than 0. Please re-enter tax %; " << endl;
+            cout << "Tax % for this location: ";
+            cin >> tax;
+            cin.get();
+        }
+    }
+    return tax;
+}
+
+int main()
+{
+    float x;
+    float taxPercentage;
+
+    cout << "\nThis program will computer a restaurant tip based on\n"
+        "the total bill amount and the % the patron wishes to "
+        "tip the server." << endl;
+
+    float tax{getTax()};
+
+    float billTotal;
+    float desiredTipRate;
+    char doIRunAgain;
+    char again;
+
+
+    cout << "**************** Tip Helper *****************\n";
+    cout << "Enter total bill amount: ";
+    cin >> billTotal;
+    cout << "Enter tip rate: ";
+    cin >> desiredTipRate;
+
+    Tips obj2;
+    obj2.computeTip(billTotal, desiredTipRate);
+
+    cout << "Compute another tip (y/n)?";
+    cin >> doIRunAgain;
+
+    if (doIRunAgain == 'y')
+    {
+        cout << "**************** Tip Helper *****************\n";
+        cout << "Enter total bill amount: ";
+        cin >> billTotal;
+        cout << "Enter tip rate: ";
+        cin >> desiredTipRate;
+
+        Tips obj2;
+        obj2.computeTip(billTotal, desiredTipRate);
+    }
+    else if (doIRunAgain == 'n')
+    {
+        cout << "Okay! Thank you for using Tip Helper. Have a pleasant day.";
+    }
+}
