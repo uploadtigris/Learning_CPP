b6bdf6bedc1a87885c6f6bef3e1a1aafbdb53804
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-10-25 10:50:25 -0500

    Working on issue of reading multiple column file.

--- Mendez_Ch8Lab.cpp
+++ Mendez_Ch8Lab.cpp
@@ -59,36 +59,55 @@ public:
     }
 };
 
-typedef int arrayType[];            //typedef
-
-void showValues(arrayType , int); //funciton prototype
+//typedef int arrayType[];            //typedef
+//void showValues(arrayType , int); //funciton prototype
 
 int main() {
-    int i;
-    const int SIZE = 7;
-    //seven PayRoll objects in an Array;
-    int obj0;
+    const int NUM_EMPLOYEES = 7;
+    int hours[NUM_EMPLOYEES];
+    int count;
+    int numberOfEmployees;
 
-    //first loop
-    ifstream item("payroll.txt");
-    if(item.is_open()) {
-        string obj0[7];
+    ifstream inputFile;
+    inputFile.open("payroll.txt");
 
-        for (int i = 0; i < 7; i++) {
-            item >> obj0[i];
-        }
+    if (!inputFile)
+    {
+        cout << "Error finding input file" << endl;
+        system("pause");
+        exit(-1);
     }
 
-    int set1[] = { 1, 2, 3, 4, 5, 6, 7};
-    showValues(set1, SIZE);
+    count = 0;
+    while (!inputFile.eof())
+    {
+        inputFile >> hours[count];
+        count++;
+    }
+    numberOfEmployees = count;
+
+    //display
+    cout << "The hours you entered are:";
+    for (count = 0; count < numberOfEmployees; count++)
+        cout << " " << hours[count];
+
+    cout << endl;
+
+    system("pause");
+    return 0;
 
 
+
+
+
+
+    //first loop
 
     //second loop; call class function for each object, to return
     //employee's gross pay (an individual's total earnings throughout
     //                      a given period before any deductions are made.)
 
-    
+
     string n = "number";
     cout << "Employee" << "   " << "Gross pay" << endl;
     cout << "========" << "   " << "=========" << endl;
@@ -104,9 +123,9 @@ int main() {
     return 0;
 }
 
-void showValues(arrayType nums, int size)
+/*void showValues(arrayType nums, int size)
 {
     for (int index = 0; index < size; index++)
         cout << nums[index] << " ";
     cout << endl;
-}
+}*/
