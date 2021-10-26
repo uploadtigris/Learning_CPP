4beb6ec4987b37cd01abd202faf754af416339ad
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-10-26 18:58:35 -0500

    Update Mendez_Ch9Lab.cpp

--- Mendez_Ch9Lab.cpp
+++ Mendez_Ch9Lab.cpp
@@ -1,1 +1,27 @@
+//###########################################################################
+// *** Assignment is not Complete ***
+//
+// Lab 8, Version 3
+// Name (Developer): Tigris Mendez
+// Class: COSC~1337
+// Proffesor: Jorgenson
+// Date: 10/21/2021
+//
+// Purpose of Program:
+//
+//
+//
+// Sorting Visualised:
+//          Sort user input-ed artist and songs into an alphabetically ordered playlist.
+//
+//      https://www.youtube.com/watch?v=kPRA0W1kECg&t=33s
+//
+//###########################################################################
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
 
