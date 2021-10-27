58dba5fb15edeecc61dd48aaff0d2ba876c58971
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-10-27 18:21:17 -0500

    finished processing and some I/O operations

--- Mendez_Ch9Lab.cpp
+++ Mendez_Ch9Lab.cpp
@@ -28,12 +28,17 @@ struct Playlist
     string title;
 };
 
+void bubbleSort(Playlist[], int);
+
 int main() {
     const int NUM_ENTRIES = 10;
     Playlist entry[NUM_ENTRIES];
     int count;
 
-    for (count = 0; count < 10; count++ )
+// The Inputs;
+//===============================================================================
+
+    for (count = 0; count < NUM_ENTRIES; count++ )
     {
         cout << "Please enter artist # " << count+1 << ": " << endl;
         getline(cin, entry[count].Artist );
@@ -41,7 +46,58 @@ int main() {
         getline(cin, entry[count].title);
     }
 
-    
+// The Outputs;
+//===============================================================================
+
+    cout << "\n\n"; cout << "The Unsorted Playlist is: " << endl;
+    cout << "========================================" << endl;
+
+    for(count = 0; count < NUM_ENTRIES; count++)
+    {
+        cout << entry[count].Artist << ", " << entry[count].title << endl;
+    }
+    system("pause");
+
+    bubbleSort(entry, NUM_ENTRIES);
+
+    cout << "\n\n"; cout << "The Sorted Playlist is: " << endl;
+    cout << "========================================" << endl;
+
+    for(count = 0; count < NUM_ENTRIES; count++)
+    {
+        cout << entry[count].Artist << ", " << entry[count].title << endl;
+    }
 
     return 0;
+}
+
+// The 'Void'
+//===============================================================================
+
+// Bubble Sort
+void bubbleSort(Playlist array[], int size)
+{
+    bool madeAswap = true;
+
+    for (int maxElement = size - 1; maxElement > 0 && madeAswap; maxElement--)
+    {
+        madeAswap = false;
+
+        for (int index = 0; index < maxElement; index++)
+        {
+            if (array[index].Artist > array[index + 1].Artist)
+            {
+                 swap(array[index], array[index + 1]);
+                 madeAswap = true;
+            }
+        }
+    }
+}
+
+// swap
+void swap(Playlist &a, Playlist &b)
+{
+    Playlist temp = a;
+    a = b;
+    b = temp;
 }
