8550b0f12876b1f691ea042a50ca8f842829e6d1
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-11-11 14:57:53 -0600

    Create Mendez_Ch10_Lab_P.A.cpp

---
+++ Mendez_Ch10_Lab_P.A.cpp
@@ -0,0 +1,35 @@
+//###########################################################################
+// *** Assignment is not yet Complete ***
+//
+// Lab 10 (A), Version 1
+// Name (Developer): Tigris Mendez
+// Class: COSC~1337
+// Proffesor: Jorgenson
+// Date: 11/1/2021
+//
+// Purpose of Program:
+//
+//
+//
+//###########################################################################
+
+#include <iostream>
+#include <algorithm> //for the sort function
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+// Pseudo Code:
+
+// Modular Functions:
+        // sort:
+
+        // find median:
+            // if len(array) is divisible by 2 (even) then 
+
+        // printArray:
+            // for loop for every subscript 'i' in array
+            // print contents of each
+            // when i == i - 1, then stop the loop.
