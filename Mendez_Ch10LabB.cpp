adffa5d0a8008e72f37cc1f3c45109713a0917e1
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-11-12 13:12:53 -0600

    Create Mendez_Ch10LabB.cpp

---
+++ Mendez_Ch10LabB.cpp
@@ -0,0 +1,13 @@
+//###########################################################################
+// *** Assignment is not Complete ***
+//
+// Lab 10 (B), Version 1
+// Name (Developer): Tigris Mendez
+// Class: COSC~1337
+// Proffesor: Jorgenson
+// Date: 11/12/2021
+//
+// Purpose of Program:
+//          
+//
+//###########################################################################
