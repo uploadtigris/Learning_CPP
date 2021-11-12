b4e1f43650600ee46ac56f1526dcaed6a170887e
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-11-12 13:41:23 -0600

    Pseudo code setup

--- Mendez_Ch10LabB.cpp
+++ Mendez_Ch10LabB.cpp
@@ -8,6 +8,39 @@
 // Date: 11/12/2021
 //
 // Purpose of Program:
-//          
+//      To find # of movies college students see per month.
+//      Asks the user how many student surveyed and dynamically allocates
+//      an array of that size.
+//      Then, the user will enter the number of movies that each student has seen.
+//      Then, the program will set the scores and calculate the average.
 //
 //###########################################################################
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+//Pseudo Code:
+
+//main
+    // cout << "number of students:"
+    //cin >> array size;
+    //create array the size of 'array size' input;
+   //print average
+    // destruct the array
+
+//Get the data
+    // obtain the number of movies watched by each college student
+    // input validation: number of movies must be positive
+
+//Sort the data
+    //sort given array in ascending order
+    //use the STL sort function
+
+//Display
+    //print the sorted array list of number of movies watched to screen.
+
+//Average
+    // Calculates the avg. of the movies watched by students and returns it to the main function.
