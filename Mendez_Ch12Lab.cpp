366a391220717c1b3aaa02f188bf5c6b738ef2b5
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-11-24 23:47:06 -0600

    Create Mendez_Ch12Lab.cpp

---
+++ Mendez_Ch12Lab.cpp
@@ -0,0 +1,21 @@
+//###########################################################################
+// *** Assignment is not Complete ***
+//
+// Lab 12, Version 1
+// Name (Developer): Tigris Mendez
+// Class: COSC~1337
+// Profesor: Jorgenson
+// Date: 11/24/2021
+//
+// Purpose of Program:
+//
+//
+//
+//###########################################################################
+#include <iostream>
+#include <cstring>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
