ac881fbbdae71845308fd423f3db1c1741ec11bb
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-11-28 16:26:06 -0600

    Update Mendez_Ch12_Lab2.cpp

--- Mendez_Ch12_Lab2.cpp
+++ Mendez_Ch12_Lab2.cpp
@@ -5,17 +5,44 @@
 // Name (Developer): Tigris Mendez
 // Class: COSC~1337
 // Profesor: Jorgenson
-// Date: 11/24/2021
+// Date: 11/27/2021
 //
 // Purpose of Program:
-//
-//
+//          Use the functions "upper, lower, and flip" accepting a C~string
+//          as an argument.
+//          The output of the function will be the original function manipulated
+//          by each of the different functions.
 //
 //###########################################################################
 #include <iostream>
 #include <cstring>
+using namespace std;
+
+void toUpper()
+{
+
+}
+
+void toLower()
+{
+
+}
+
+void toSwitch()
+{
+    
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Welcome to the Case Manipulator program!" << endl;
+    cout << "Please enter a sentence of text of text" << endl;
+
+    char sentence[256], newsentence[256];
+
+    cin.getline(sentence, 256);
+
+
+
+    cout << sentence;
     return 0;
 }
