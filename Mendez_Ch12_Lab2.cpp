90d6df209c789e25496bd98fe2e7db95ec21df75
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-11-28 17:10:06 -0600

    Filled out the functions and completed the processes and function calls.

--- Mendez_Ch12_Lab2.cpp
+++ Mendez_Ch12_Lab2.cpp
@@ -1,5 +1,5 @@
 //###########################################################################
-// *** Assignment is not Complete ***
+// *** Assignment is Complete ***
 //
 // Lab 12, Part 2
 // Name (Developer): Tigris Mendez
@@ -18,31 +18,74 @@
 #include <cstring>
 using namespace std;
 
-void toUpper()
+void upper(char arr[])
 {
-
+    unsigned long int len = strlen(arr);
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = toupper(arr[i]);
+    }
 }
 
-void toLower()
+void lower(char arr[])
 {
-
+    unsigned long int len = strlen(arr);
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = tolower(arr[i]);
+    }
 }
 
-void toSwitch()
+void flip(char arr[])
 {
-    
+    unsigned long int len = strlen(arr);
+    for (int i = 0; i < len; i++)
+    {
+        if(isupper(arr[i]))
+        {
+            arr[i] = tolower(arr[i]);
+        }
+        else if (islower(arr[i]))
+        {
+            arr[i] = toupper(arr[i]);
+        }
+    }
 }
 
 int main() {
     cout << "Welcome to the Case Manipulator program!" << endl;
     cout << "Please enter a sentence of text of text" << endl;
 
-    char sentence[256], newsentence[256];
+    char sentence[256], flipSentence[256], upperSentence[256], lowerSentence[256];
 
     cin.getline(sentence, 256);
 
+    strcpy(upperSentence, sentence);
+    strcpy(lowerSentence, sentence);
+    strcpy(flipSentence, sentence);
+
+    upper(upperSentence);
+    lower(lowerSentence);
+    flip(flipSentence);
 
 
-    cout << sentence;
+
+    cout << "You entered the sentence: " << "'" << sentence << "'" << endl;
+    cout << upperSentence << endl;
+    cout << lowerSentence << endl;
+    cout << flipSentence << endl;
+
     return 0;
 }
+
+//Pseudo Code
+//  upper:
+//      take ever char in sentence and convert it to an upper case letter;
+//
+//  lower:
+//      take every char in sentence and convert it to an a lower letter.
+//
+//  toSwitch:
+//      for every char in sentence
+//          if char is lower, make upper
+//          if char is upper, make lower.
