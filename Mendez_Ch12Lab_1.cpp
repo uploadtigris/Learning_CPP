2134821f40deda3a6893be34997591b68d8f6b60
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-11-28 16:14:25 -0600

    Reformatted and finished

--- Mendez_Ch12Lab_1.cpp
+++ Mendez_Ch12Lab_1.cpp
@@ -1,5 +1,5 @@
 //###########################################################################
-// *** Assignment is not Complete ***
+// *** Assignment is Complete ***
 //
 // Lab 12, Part 1
 // Name (Developer): Tigris Mendez
@@ -14,42 +14,63 @@
 //
 //###########################################################################
 #include <iostream>
-#include <cstring>
 #include <string>
 using namespace std;
 
 int main() {
+    int size;
+    int newsize;
+    int newnewsize;
+    string sentence;
+    string newSentence;
+    string newnewSentence;
+    string finalSentence;
+    string firstChar;
+
     cout << "Hello! Welcome to the Word Separator program" << endl;
     cout << "Please enter text as a running total of words w/o any spaces below..." << endl;
 
-    string sentence;
     //prompt user for input
     getline(cin, sentence);
-    string newSentence;
 
-    int size;
+    //Separates words with spaces
     size = sentence.length();
-    int n = 0;
-
-    //function
-    for(int i=0; i < size; i++){
-        if (!islower(sentence[i])) {
+    for(int i=0; i < size; i++)
+    {
+        if (!islower(sentence[i]))
+        {
             newSentence += " ";
         }
         newSentence += sentence[i];
     }
 
-        cout << "The Sentence you input was" << sentence << endl;
-        cout << newSentence;
+    // locate everything in sentence except the first character
+    newsize = newSentence.length();
+    for(int i=1; i < newsize; i++)
+    {
+        newnewSentence += newSentence[i+1];
+    }
+
+    //first character
+    firstChar = sentence[0];
+
+    //make the separated sentence all lower case
+    newnewsize = newnewSentence.length();
+    for(int n=0; n < newnewsize; n++)
+    {
+        newnewSentence[n] = tolower(newnewSentence[n]);
+    }
+
+    //concatenate;
+    finalSentence = firstChar + newnewSentence;
+
+    cout << "The Sentence you input was: " << "'"<< sentence << "'" << endl;
+    cout << finalSentence << endl;
 
     return 0;
 }
 
-//Pseudo code:
-//
-//Main ~>
-//      Prompt user to enter a string as all one word with caps
-//      for every index "i" in the array, search to see if that indexed char isupper
-//      if upper, cout a space before
-//
-//      output the new array
+// Output Testers
+//cout << newSentence << endl;
+//cout << newnewSentence << endl;
+//cout << firstChar << endl;
