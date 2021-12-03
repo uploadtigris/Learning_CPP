384c6fdd5b55c56c3cf6b008402a10936791ab5a
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-12-03 16:23:20 -0600

    Update Mendez_Lab13.cpp

--- Mendez_Lab13.cpp
+++ Mendez_Lab13.cpp
@@ -1,19 +1,3 @@
-//###########################################################################
-// *** Assignment is Not Complete ***
-//
-// Lab 13, Part 1 + 2
-// Name (Developer): Tigris Mendez
-// Class: COSC~1337
-// Profesor: Jorgenson
-// Date: 12/03/2021
-//
-// Purpose of Program:
-//          This program is meant to encrypt a .txt documents contents by
-//          by simply adding or subtracting from the ASCII values of each
-//          character.
-//
-//###########################################################################
-
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -23,14 +7,47 @@ void testBits(fstream &);
 
 int main() {
 
-    int ans, key;
+    int ans;
+    string in;
     cout << "Press 1 to Encrypt file.\n Press 2 to Decrypt file." << endl;
     cin >> ans;
-    fstream buckyFile;
-    buckyFile.open("tuna.txt", fstream::out | fstream::in);
-    testBits(buckyFile);
-    buckyFile << "I love tuna";
-    buckyFile.close();
+
+    if(ans == 1)
+    {
+        char c;
+        ifstream buckyFile;
+        ofstream fout;
+        buckyFile.open("tuna.txt", fstream::out | fstream::in | ios::binary);
+        in = "output";
+        fout.open("tuna2.txt", fstream::out | fstream::in | ios::binary);
+        while(!buckyFile.eof())
+        {
+            buckyFile >> noskipws >> c;
+            int temp = (c + 10);
+            fout << (char)temp;
+        }
+        buckyFile.close();
+        fout.close();
+    }
+    else if (ans == 2)
+    {
+        char c;
+        ifstream buckyFile;
+        ofstream fout;
+        buckyFile.open("tuna.txt", fstream::out | fstream::in | ios::binary);
+        in = "decrpyt_output";
+        fout.open("tuna2.txt", fstream::out | fstream::in | ios::binary);
+        while(!buckyFile.eof())
+        {
+            buckyFile >> noskipws >> c;
+            int temp = (c - 10);
+            fout << (char)temp;
+        }
+        buckyFile.close();
+        fout.close();
+    }
+
+
 
     return 0;
 }
@@ -44,7 +61,7 @@ void testBits(fstream& buckyFile)
 
 }
  //Pseudo Code:
-    // prompt a user to 
+    // prompt a user to
     //'open' new file
     // read contents of the file into buffer
     // add '10' to the ASCII values for each character in the char array.
